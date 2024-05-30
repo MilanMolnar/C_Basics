5ae937894e94e3eca19cf72d72ec574c37540bfb
Molnar Milan <molnarmilan.cc@gmail.com>
2024-05-30 18:53:55 +0200

    Reading files

--- src/FileHandling.c
+++ src/FileHandling.c
@@ -3,9 +3,19 @@
 
 int main()
 {
-    FILE * fpointer = fopen("file.txt", "w"); // w, r, a
-    fprintf(fpointer, "Jim, CS\nTom, Mc");
+    FILE * fWritepointer = fopen("file.txt", "w"); // w, r, a
+    fprintf(fWritepointer, "Jim, CS\nTom, Mc");
 
-    fclose(fpointer); //closing file by pointer
+    fclose(fWritepointer); //closing file by pointer
+
+    FILE * fReadpointer = fopen("file.txt", "r");
+
+    char line[255];
+
+    fgets(line, 255, fReadpointer); // afeter use the pointer is incremented-
+    fgets(line, 255, fReadpointer); // now fReadpointer is pointing to the next line
+
+    printf("%s", line);
+    fclose(fReadpointer);
     return 0;
 }
